29a0c11d9fafb298735374fb3258078515958314
Bogdan Covrig <bogcov@gmail.com>
2023-05-09 13:24:38 +0200

    chore: Add Vercel SSR on Edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import vercelEdge from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: "server",
+  adapter: vercelEdge(),
 });
